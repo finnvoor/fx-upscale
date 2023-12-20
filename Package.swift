22b884dc203b5def8debcf81b11c5c9ad329a02c
Wolfgang Lutz <WLBORg@gmx.de>
2023-12-20 21:34:17 +0100

    Add Executable Product to allow running with mint (#1)

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 let package = Package(
     name: "MetalFXUpscale",
     platforms: [.macOS(.v13)],
+    products: [
+        .executable(name: "MetalFXUpscale", targets: ["MetalFXUpscale"]),
+    ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0")
     ],
