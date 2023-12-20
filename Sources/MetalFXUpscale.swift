f45e009d2a001ac6d2ea02233be2a2ce9f0db31b
Finn Voorhees <finnvoorhees@gmail.com>
2023-12-20 11:23:31 -0500

    Initial Commit

---
+++ Sources/MetalFXUpscale.swift
@@ -0,0 +1,14 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+// 
+// Swift Argument Parser
+// https://swiftpackageindex.com/apple/swift-argument-parser/documentation
+
+import ArgumentParser
+
+@main
+struct MetalFXUpscale: ParsableCommand {
+    mutating func run() throws {
+        print("Hello, world!")
+    }
+}
