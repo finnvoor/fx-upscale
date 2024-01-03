b9290623ead76f211e38c278b5b4551666ec216e
Finn Voorhees <finnvoorhees@gmail.com>
2024-01-03 17:32:04 -0500

    Update schemes

--- Tests/UpscalingTests/UpscalingTests.swift
+++ Tests/UpscalingTests/UpscalingTests.swift
@@ -2,37 +2,44 @@
 import XCTest
 
 final class UpscalingTests: XCTestCase {
-    func testBasicUpscale() {
+    func testBasicUpscale() async throws {
         // basic upscale
+        throw XCTSkip("unimplemented")
     }
 
     /// https://github.com/finnvoor/fx-upscale/issues/8
-    func testUpscaleTransformedVideo() {
+    func testUpscaleTransformedVideo() async throws {
         // make sure transform is applied to upscaled video
+        throw XCTSkip("unimplemented")
     }
 
     /// https://github.com/finnvoor/fx-upscale/commit/e6666afdb9a5ce1eda38437bec25b0743b740360
-    func testUpscaleMissingColorInfo() {
+    func testUpscaleMissingColorInfo() async throws {
         // make sure it doesn't crash
+        throw XCTSkip("unimplemented")
     }
 
     /// https://github.com/finnvoor/fx-upscale/commit/44463975e46ee7d418ad41017782c1e267205c82
-    func testTranscodeToProRes() {
+    func testTranscodeToProRes() async throws {
         // >4k converted to pro res
+        throw XCTSkip("unimplemented")
     }
 
     /// https://github.com/finnvoor/fx-upscale/issues/7
-    func testAudioFormatMaintained() {
+    func testAudioFormatMaintained() async throws {
         // keep audio format
+        throw XCTSkip("unimplemented")
     }
 
     /// https://github.com/finnvoor/fx-upscale/issues/6
-    func testMaintainMetadata() {
+    func testMaintainMetadata() async throws {
         // maintain metadata/captions
+        throw XCTSkip("unimplemented")
     }
 
     /// https://github.com/finnvoor/fx-upscale/issues/4
-    func testExportProgress() {
+    func testExportProgress() async throws {
         // ensure progress works
+        throw XCTSkip("unimplemented")
     }
 }
